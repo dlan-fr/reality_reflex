54c0f7b21843a1a310c996b58caf01daaf38b655
dlan <dlan@uncovergame.com>
2015-10-16 22:19:14 +0200

    ajout code de base switch niveaux

---
+++ Assets/GameDataMngr.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameDataMngr {
+
+	private string CurrentLevel = "scene_1";
+	private string data = "balal";
+
+	public string odata
+	{
+		get
+		{
+			return data;
+		}
+	}
+
+	private static GameDataMngr _singleton = null;
+
+	public static GameDataMngr Singleton
+	{
+		get
+		{
+			if(_singleton == null)
+				_singleton = new GameDataMngr();
+
+			return _singleton;
+		}
+	}
+
+	public void setdata(string data)
+	{
+		this.data = data;
+	}
+
+	public void SetNewLevel(string newlevel)
+	{
+		this.CurrentLevel = newlevel;
+		Application.LoadLevel(newlevel);
+	}
+
+
+
+}
---
+++ Assets/TriggerPortal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPortal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		GameDataMngr.Singleton.SetNewLevel("scene_2");
+	}
+
+}
