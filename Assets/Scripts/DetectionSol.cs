48aab71e8efc19e7df5beb8fa4850aa42b6ae0b8
Cocooptique <corentin.mercier@insitutoptique>
2015-10-17 18:53:03 +0200

    marchepas

---
+++ Assets/Scripts/DetectionSol.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectionSol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnEnterCollision2D(Collision2D coll)
+	{
+		GameDataMngr.Singleton.collision = true;
+		Debug.Log("sol");
+	}
+	
+	void OnStayCollision2D(Collision2D coll)
+	{
+		GameDataMngr.Singleton.collision = true;
+		
+		Debug.Log("sols");
+	}
+	
+	void OnExitCollision2D(Collision2D coll)
+	{
+		GameDataMngr.Singleton.collision = false;
+		
+		Debug.Log("solq");
+	}
+}
