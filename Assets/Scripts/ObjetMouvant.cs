719be1fa99956ef2fec0728a969911fa4e092d26
Cocooptique <corentin.mercier@insitutoptique>
2015-10-17 13:57:57 +0200

    PlateFormesVerticales

--- Assets/Scripts/ObjetMouvant.cs
+++ Assets/Scripts/ObjetMouvant.cs
@@ -6,20 +6,43 @@ public class ObjetMouvant : MonoBehaviour {
 	public Vector3 depart;
 	public Vector3 arrivee;
 	
+	public enum SensMouvement
+    {
+        Horizontal,
+        Vertical
+    };
+	public SensMouvement sens;
+	
 	private bool direction;
 	private float prop;
 	private float condition;
 	private float distancex;
+	private float distancey;
 	
 	// Use this for initialization
 	void Start () {
 		direction = false;
-		prop=(arrivee.y-depart.y)/((arrivee.x-depart.x)/speed);
 		Vector3 d = new Vector3();
-		distancex = speed/(arrivee.x-depart.x);
-		condition = distancex;
-		d.x += speed;
-		d.y += prop;
+		switch (sens){
+			case SensMouvement.Horizontal:
+			{
+				prop=(arrivee.y-depart.y)/((arrivee.x-depart.x)/speed);
+				distancex = speed/(arrivee.x-depart.x);
+				condition = distancex;
+				d.x += speed;
+				d.y += prop;
+				break;
+			}
+			case SensMouvement.Vertical:
+			{
+				prop=(arrivee.x-depart.x)/((arrivee.y-depart.y)/speed);
+				distancey = speed/(arrivee.y-depart.y);
+				condition = distancey;
+				d.y += speed;
+				d.x += prop;
+				break;
+			}
+		}
 		transform.position=depart+d;
 	
 	}
@@ -27,21 +50,50 @@ public class ObjetMouvant : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 deplac = new Vector3();
-		distancex = speed/(arrivee.x-depart.x);
-		if ((condition<distancex)||(condition>(1-distancex))) {
-			direction=!direction;
+		switch(sens){
+			case SensMouvement.Horizontal:
+			{
+				distancex = speed/(arrivee.x-depart.x);
+				if ((condition<distancex)||(condition>(1-distancex))) 
+				{
+					direction=!direction;
+				}
+				if (direction) 
+				{
+					deplac.x += speed;
+					deplac.y += prop;
+					condition+=distancex;
+				}
+				else 
+				{
+					deplac.x -= speed;
+					deplac.y -= prop;
+					condition-=distancex;
+				}
+				break;
+			}
+			case SensMouvement.Vertical:
+			{
+				distancey = speed/(arrivee.y-depart.y);
+				if ((condition<distancey)||(condition>(1-distancey))) 
+				{
+					direction=!direction;
+				}
+				if (direction) 
+				{
+					deplac.y += speed;
+					deplac.x += prop;
+					condition+=distancey;
+				}
+				else 
+				{
+					deplac.y -= speed;
+					deplac.x -= prop;
+					condition-=distancey;
+				}
+				break;
+			}
 		}
-		if (direction) {
-			deplac.x += speed;
-			deplac.y += prop;
-			condition+=distancex;
-		}
-		else {
-			deplac.x -= speed;
-			deplac.y -= prop;
-			condition-=distancex;
-		}
-		
 		transform.position += deplac;
 	}
 	
