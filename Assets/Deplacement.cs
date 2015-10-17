66b06fea8f001792acd7a71c01ba0e6036ae57fe
Cocooptique <corentin.mercier@insitutoptique>
2015-10-17 10:33:43 +0200

    Plates FormesV3

--- Assets/Deplacement.cs
+++ Assets/Deplacement.cs
@@ -5,8 +5,6 @@ public class Deplacement : MonoBehaviour {
 	public float speed;
 	public float jump;
 	
-	private Vector3 zeroPlateForme;
-	private Vector3 decalage = Vector3.zero;
 	
 	// Use this for initialization
 	void Start () {
@@ -29,32 +27,19 @@ public class Deplacement : MonoBehaviour {
 				if (Input.GetKey (KeyCode.Z)) {
 						deplac.y += jump;
 				}
-				deplac+=decalage;
- 
                 transform.position += deplac;
 	}
 
 	
 	void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.tag=="PF"){
-			zeroPlateForme=coll.transform.position;
-			this.GetComponent<Rigidbody2D>().gravityScale=0;
-			Debug.Log("Collision");
-		}
-	}
-	
-	void OnCollisionStay2D(Collision2D coll){
-		if (coll.gameObject.tag=="PF"){
-			decalage=coll.transform.position-zeroPlateForme;
-			zeroPlateForme=coll.transform.position;
+			transform.parent=coll.transform;
 		}
 	}
 	
 	void OnCollisionExit2D(Collision2D coll){
 		if (coll.gameObject.tag=="PF"){
-			zeroPlateForme=Vector3.zero;
-			this.GetComponent<Rigidbody2D>().gravityScale=1;
-			decalage=Vector3.zero;
+			transform.parent=null;
 		}
 	}
 }
